7a5229d6a45b17f185305b541cb022c41d457ca2
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-09-13 11:55:49 +0530

    adding sum value

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // do some load intensive work
+    let sum = 0;
     for (let i = 0; i < 10000; i++) {
         // do nothing
+        sum += i;
     }
-    res.send(`<center> <h1>Load Balancing Test with NGINX. Current Server PORT : ${port}</h1> </center>`);
+    res.send(`<center> <h1>Load Balancing Test with NGINX. Current Server PORT : ${port}, and sum is ${sum} </h1> </center>`);
 });
 
 app.listen(port, () => {
