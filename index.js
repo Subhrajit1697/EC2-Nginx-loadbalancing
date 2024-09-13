67b7ed6ef499fc3f2d930b027e3ee9889310df2d
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-09-13 11:43:28 +0530

    increase load

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
+    // do some load intensive work
+    for (let i = 0; i < 10000; i++) {
+        // do nothing
+    }
     res.send(`<center> <h1>Load Balancing Test with NGINX. Current Server PORT : ${port}</h1> </center>`);
 });
 
