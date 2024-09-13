bf3a1f28098ec0b3f189e9c8797f5ca75ce9f027
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-09-13 15:56:42 +0530

    final update

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    console.log('port:', port);
+    next();
+});
 
 app.get('/', (req, res) => {
     // do some load intensive work
     let sum = 0;
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 1000000; i++) {
         // do nothing
         sum += i;
     }
+    console.log(`sum is ${sum}`);
     res.send(`<center> <h1>Load Balancing Test with NGINX. Current Server PORT : ${port}, and sum is ${sum} </h1> </center>`);
 });
 
